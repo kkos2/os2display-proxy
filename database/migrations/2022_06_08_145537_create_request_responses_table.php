de1fa6fac86a1898651b3c4647b8d3f1ac29d0e3
Kasper Garnaes <kasper.garnaes@gmail.com>
2022-06-09 16:51:42 +0200

    Add id column to request table
    It is required for updateOrCreate to work. Our nontrivial unique key
    is not enough.

--- database/migrations/2022_06_08_145537_create_request_responses_table.php
+++ database/migrations/2022_06_08_145537_create_request_responses_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::create('request_responses', function (Blueprint $table) {
+            $table->id();
             $table->string('path');
             $table->string('content_type');
             $table->binary('data');
