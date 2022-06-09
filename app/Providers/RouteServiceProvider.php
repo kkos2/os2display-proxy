5e8abbaa4c0837b90996c0d09c1c795079a63009
Kasper Garnaes <kasper.garnaes@gmail.com>
2022-06-09 10:54:45 +0200

    Setup routing for request/responses
    Replace default api and web route files with routes in the route 
    setup in the route service provider.
    
    This makes more sense for us since our controller should accept
    requests for any route.

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Http\Controllers\RequestResponseController;
 use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
 use Illuminate\Http\Request;
@@ -28,14 +29,12 @@ class RouteServiceProvider extends ServiceProvider
     {
         $this->configureRateLimiting();
 
-        $this->routes(function () {
-            Route::middleware('api')
-                ->prefix('api')
-                ->group(base_path('routes/api.php'));
-
-            Route::middleware('web')
-                ->group(base_path('routes/web.php'));
-        });
+        Route::get('/{request}',
+            [RequestResponseController::class, 'get'])
+            ->where('request', '.+');
+        Route::post('/{request}',
+            [RequestResponseController::class, 'create'])
+            ->where('request', '.+');
     }
 
     /**
